24a265dc3c335791d430ff86c58ed241788287b1
Soran Raof <soranraof@hotmail.com>
2023-07-22 13:49:59 +0100

    MongoDB connection

--- task-manager-app/app.js
+++ task-manager-app/app.js
@@ -1,7 +1,9 @@
+require("./db/connection");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
+const connectDB = require("./db/connection");
+require("dotenv").config();
 //middleware
 app.use(express.json());
 
@@ -24,4 +26,13 @@ app.delete("/api/tasks/:id"); //delete task
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
