18a965ec1bcc9d2fb7ace6185b8fdcb9a2ed696f
Soran Raof <soranraof@hotmail.com>
2023-07-23 19:25:04 +0100

    error handling middleware and refactoring model with async wrapper and custom errors

--- task-manager-app/app.js
+++ task-manager-app/app.js
@@ -1,38 +1,27 @@
-require("./db/connection");
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const tasks = require("./routes/task-routes");
 const connectDB = require("./db/connection");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-//middleware
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
+app.use(cors());
 
-//routes
-
+// Set up task-related routes
 app.use("/api/tasks", tasks);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-// app.get("/api/tasks"); get all tasks
-
-// app.post("/api/tasks"); create task
-
-// app.get("/api/tasks/:id"); get single task
-
-// app.patch("api/tasks/:id"); update task
-
-// app.delete("/api/tasks/:id"); delete task
-
-const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening on port ${port}...`));
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
